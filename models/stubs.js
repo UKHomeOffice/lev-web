e3e9aeb68e8854ccd93e3db23e85afab3587ae23
JoeChapman <joseph.philip.chapman@gmail.com>
2015-12-01 12:41:50 +0000

    add model stub

---
+++ models/stubs.js
@@ -0,0 +1,95 @@
+'use strict';
+
+module.exports = [{
+  'system-number': '12345',
+  forenames: 'John',
+  surname: 'Smith',
+  dob: '11/12/1983',
+  'birth-place': 'Oxford',
+  nationality: 'Croatian',
+  gender: 'Male',
+  father: {
+    name: 'Robert Adam Smith',
+    occupation: 'Welder'
+  },
+  mother: {
+    name: 'Anne Catherine Smith',
+    occupation: 'Doctor',
+    neenee: 'Bob'
+  },
+  registered: {
+    'jointly': 'Yes',
+    'district': 'Oxfordshire',
+    'sub-district': 'Cherwell',
+    'admin-area': 'Oxfordshire'
+  }
+}, {
+  'system-number': '98765',
+  forenames: 'John Francis',
+  surname: 'Smith',
+  dob: '23/09/1976',
+  'birth-place': 'Coltishall',
+  nationality: 'France',
+  gender: 'Male',
+  father: {
+    name: 'Paul Davis',
+    occupation: 'Self-employed'
+  },
+  mother: {
+    name: 'Janet Smith',
+    occupation: 'Engineer',
+    nee: 'Alice'
+  },
+  registered: {
+    'jointly': 'No',
+    'district': 'Norfolk',
+    'sub-district': 'Norwich',
+    'admin-area': 'Norfolk'
+  }
+}, {
+  'system-number': '00001',
+  forenames: 'John',
+  surname: 'Smith',
+  dob: '11/02/1980',
+  'birth-place': 'Tonbridge',
+  nationality: 'English',
+  gender: 'Male',
+  father: {
+    name: 'Alan Monk',
+    occupation: 'Shoe shine'
+  },
+  mother: {
+    name: 'Pauline Smith',
+    occupation: 'Cleaner',
+    nee: 'Tony'
+  },
+  registered: {
+    'jointly': 'Yes',
+    'district': 'Kent',
+    'sub-district': 'Medway',
+    'admin-area': 'Kent'
+  },
+}, {
+  'system-number': '99999',
+  forenames: 'Sarah',
+  surname: 'Marshall',
+  dob: '04/02/1979',
+  'birth-place': 'Brighton',
+  nationality: 'English',
+  gender: 'Female',
+  father: {
+    name: 'Kanye Marshall',
+    occupation: 'Biologist'
+  },
+  mother: {
+    name: 'Louise Campell Marshall',
+    occupation: 'Hat maker',
+    nee: 'Campbell'
+  },
+  registered: {
+    'jointly': 'Yes',
+    'district': 'Sussex',
+    'sub-district': 'Brighton & Hove',
+    'admin-area': 'Sussex'
+  }
+}];
