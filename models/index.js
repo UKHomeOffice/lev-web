207b84c547da79d64fadcaf22559454cdd0394e0
JoeChapman <joseph.philip.chapman@gmail.com>
2015-12-01 13:32:19 +0000

    update model with methods and test

--- models/index.js
+++ models/index.js
@@ -1,20 +1,33 @@
 'use strict';
 
-var Model = module.exports = function Model(attrs) {
-  this.attributes = attrs;
+var _ = require('underscore');
+var stubs = require('./stubs');
+
+var Model = module.exports = function Model(attr) {
+  this.attributes = _.pick(attr, _.identity);
 };
 
-Model.prototype.get = function getRecords() {
+Model.prototype.read = function getRecords() {
   return new Promise(function returnRecords(resolve, reject) {
-    if (typeof resolve === 'function') {
+    var records = _.where(stubs, this.toJSON());
+    if (records) {
       resolve({
-        records: [{
-          forenames: 'John',
-          surname: 'Adams'
-        }]
+        records: records
       });
     } else {
-      reject(new Error('BIG ERROR'));
+      reject(new Error('No records available'));
     }
-  });
+  }.bind(this));
+};
+
+Model.prototype.set = function set(id, value) {
+  this.attributes[id] = value;
+};
+
+Model.prototype.get = function get(id) {
+  return this.attributes[id];
+};
+
+Model.prototype.toJSON = function toJSON() {
+  return this.attributes;
 };
