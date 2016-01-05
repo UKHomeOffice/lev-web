70ef2fd5fabbbbcaa2a690a77644d3cfa4b2b4e3
JoeChapman <joseph.philip.chapman@gmail.com>
2016-01-05 17:02:17 +0000

    LEVWEB-77: Update links to point at correct url
    - update href values in edit and new search button/link
    - add acceptance tests for clicking the link

--- test/acceptance/results.js
+++ test/acceptance/results.js
@@ -29,36 +29,67 @@ describe('Results page @watch', function() {
   describe('When there is more than one result', function () {
 
     beforeEach(function () {
-        browser.setValue('input[name="surname"]', 'Smith')
-        browser.setValue('input[name="forenames"]', 'John')
-        browser.submitForm('form');
-      });
+      browser.setValue('input[name="surname"]', 'Smith')
+      browser.setValue('input[name="forenames"]', 'John')
+      browser.submitForm('form');
+    });
 
-      it('redirects to the results page', function () {
-        browser.getUrl().should.contain('/results');
-      });
+    it('redirects to the results page', function () {
+      browser.getUrl().should.contain('/results');
+    });
 
-      it('displays an appropriate message', function () {
-        browser.getText('h1').should.equal('3 records found for John Smith');
-      });
+    it('displays an appropriate message', function () {
+      browser.getText('h1').should.equal('3 records found for John Smith');
+    });
 
-      it('displays a subset of each record in a list', function () {
-        browser.getText('#records li:first-child tr')
-          .should.deep.equal([
-            'Place of birth Oxford',
-            'Father Robert Adam Smith',
-            'Mother Anne Catherine Smith'
-          ]);
+    it('displays a subset of each record in a list', function () {
+      browser.getText('#records li:first-child tr')
+        .should.deep.equal([
+          'Place of birth Oxford',
+          'Father Robert Adam Smith',
+          'Mother Anne Catherine Smith'
+        ]);
 
-        browser.getText('#records li:last-child tr')
-          .should.deep.equal([
-            'Place of birth Tonbridge',
-            'Father Alan Monk',
-            'Mother Pauline Smith'
-          ]);
+      browser.getText('#records li:last-child tr')
+        .should.deep.equal([
+          'Place of birth Tonbridge',
+          'Father Alan Monk',
+          'Mother Pauline Smith'
+        ]);
 
-      });
+    });
 
+  });
+
+  describe('When I select the "New search" button', function () {
+    beforeEach(function () {
+      browser.setValue('input[name="surname"]', 'Smith');
+      browser.setValue('input[name="forenames"]', 'John');
+      browser.submitForm('form');
+      browser.click('#newSearchLink');
+    });
+    it('redirects to the search page', function () {
+      browser.getUrl().should.contain('/search');
+    });
+  });
+
+  describe('When I select the "Edit search" link', function () {
+    beforeEach(function () {
+      browser.setValue('input[name="surname"]', 'Smith');
+      browser.setValue('input[name="forenames"]', 'John');
+      browser.submitForm('form');
+      browser.click('#editSearchLink');
+    });
+    it('redirects to the search page', function () {
+      browser.getUrl().should.contain('/search');
+    });
+    it('has the correct form values', function () {
+      browser.getValue('#content form > div:nth-child(2) > input').should.equal('Smith');
+      browser.waitForVisible('input[name="surname"]', 5000);
+
+      browser.getValue('#content form > div:nth-child(3) > input').should.equal('John');
+      browser.waitForVisible('input[name="forenames"]', 5000);
+    });
   });
 
 });
