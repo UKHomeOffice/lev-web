c1669c6075065a182d97dce50d4d6bc60b338982
Daniel A.C. Martin <github@daniel-martin.co.uk>
2016-04-19 12:03:45 +0100

    [LEVWEB-162] Bugfix: Set path when deleting cookie

--- controllers/logged-out.js
+++ controllers/logged-out.js
@@ -2,7 +2,7 @@
 
 module.exports = function renderLoggedOut(req, res) {
   // FIXME: Delete once no longer needed!
-  res.clearCookie('kc-access');
+  res.clearCookie('kc-access', {path: '/'});
 
   // Render page
   res.render('pages/logged-out');
