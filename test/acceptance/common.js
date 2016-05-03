a43acd3d5e2d5e252ab174b933f4be52a25baf6c
Tim Gent <tim.gent@gmail.com>
2016-05-03 17:37:41 +0100

    E2E tests run in docker container And some bug fixes on the tests

--- test/acceptance/common.js
+++ test/acceptance/common.js
@@ -4,18 +4,26 @@ var mockProxy = require('./mock-proxy');
 var testConfig = require('./config');
 
 before(function () {
+  if (testConfig.env === 'local') {
+    mockProxy.listen();
+  }
+});
+
+beforeEach(function () {
   if (testConfig.env !== 'local') {
     doLogin();
-  } else {
-    mockProxy.listen();
+  }
+});
+
+afterEach(function () {
+  if (testConfig.env !== 'local' && browser.isExisting('#logout')) {
+    browser.click('#logout');
   }
 });
 
 function doLogin() {
   browser.url(testConfig.url);
-  console.log('Attempting login....');
   browser.setValue('input[name="username"]', testConfig.username);
   browser.setValue('input[name="password"]', testConfig.password);
   browser.submitForm('form');
-  console.log('Login successful')
 }
