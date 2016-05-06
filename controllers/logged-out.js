ecd65b3a7b7a212291ea480a51dd9fe495d53aa0
Daniel A.C. Martin <github@daniel-martin.co.uk>
2016-05-06 16:54:01 +0100

    [LEVWEB-258] Use proper logout endpoint
    Makes use of the dedicated logout endpoint provided by newer versions of
    keycloak-proxy. Eliminates the need for us to delete its cookie
    ourselves.

--- controllers/logged-out.js
+++ controllers/logged-out.js
@@ -1,12 +1,5 @@
 'use strict';
 
 module.exports = function renderLoggedOut(req, res) {
-  // FIXME: Delete once no longer needed!
-  res.clearCookie('kc-access', {
-    domain: '.' + req.get('host'),
-    path: '/'
-  });
-
-  // Render page
   res.render('pages/logged-out');
 };
