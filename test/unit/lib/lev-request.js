525f79a2e263691013741ce38706df33613fdfb8
Daniel A.C. Martin <github@daniel-martin.co.uk>
2016-06-23 20:54:14 +0100

    SQUASHME: Re-factor

---
+++ test/unit/lib/lev-request.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var proxyquire = require('proxyquire');
+
+describe('lib/lev-request', function () {
+  describe('get', function () {
+    var requestGet;
+    var levRequest;
+    var fsReadFileSync;
+    var config;
+
+    beforeEach(sinon.test(function () {
+      config = {};
+
+      requestGet = this.stub();
+
+      fsReadFileSync = sinon.stub();
+
+      levRequest = proxyquire('../../../lib/lev-request', {
+        fs: {
+          readFileSync: fsReadFileSync
+        },
+        './request-with-oauth2': {
+          get: requestGet
+        },
+        '../config': config
+      });
+    }));
+
+    it('Adds config for mutual TLS when available', () => {
+      config.lev_tls = {
+        key: 'TLS Key',
+        cert: 'TLS Cert',
+        ca: 'TLS CA'
+      };
+
+      fsReadFileSync.returnsArg(0);
+
+      levRequest.get('http://testhost.com');
+
+      requestGet.should.have.been.calledWith({
+        url: 'http://testhost.com',
+        key: 'TLS Key',
+        cert: 'TLS Cert',
+        ca: 'TLS CA'
+      });
+    });
+  });
+});
