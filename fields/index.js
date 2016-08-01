d894730ff65a76e99fc9c280fa5f807c7ec26c01
Robin Knipe <robin.knipe@gmail.com>
2016-08-01 12:15:24 +0100

    Add extra validation for the date of birth field

--- fields/index.js
+++ fields/index.js
@@ -25,7 +25,10 @@ module.exports = {
     }
   },
   'dob': {
-    validate: ['british-date'],
+    validate: ['british-date', 'past', {
+      type: 'since',
+      arguments: [require('moment')('1/1/1990', 'DD/MM/YYYY')]
+    }],
     dependent: {
       value: '',
       field: 'system-number'
