819059d1aa15942af06a1ff561d5ad70dbdeca23
Robin Knipe <robin.knipe@gmail.com>
2016-08-10 16:36:38 +0100

    Refactor and correct linting issues
    General minor linting fixes.
    Refactored a multi-line comment into the proper format, favoured string interpolation over concatenation.

--- test/acceptance/mock-kc-proxy/index.js
+++ test/acceptance/mock-kc-proxy/index.js
@@ -8,26 +8,28 @@ const proxy = (fromHost, fromPort, toHost, toPort, user) => {
   // Create a proxy server with custom application logic
   const myProxy = httpProxy.createProxyServer({});
 
-  // To modify the proxy connection before data is sent, you can listen
-  // for the 'proxyReq' event. When the event is fired, you will receive
-  // the following arguments:
-  // (http.ClientRequest proxyReq, http.IncomingMessage req,
-  //  http.ServerResponse res, Object options). This mechanism is useful when
-  // you need to modify the proxy request before the proxy connection
-  // is made to the target.
-  myProxy.on('proxyReq', (proxyReq, req, res, options) => {
+  /**
+   * To modify the proxy connection before data is sent, you can listen
+   * for the 'proxyReq' event. When the event is fired, you will receive
+   * the following arguments:
+   * (http.ClientRequest proxyReq, http.IncomingMessage req,
+   *  http.ServerResponse res, Object options). This mechanism is useful
+   * when you need to modify the proxy request before the proxy
+   * connection is made to the target.
+   */
+  myProxy.on('proxyReq', (proxyReq) => {
     proxyReq.setHeader('X-Auth-Username', user);
   });
 
   const server = http.createServer((req, res) => {
-    // You can define here your custom logic to handle the request
-    // and then proxy the request.
-    myProxy.web(req, res, {
-      target: 'http://' + toHost + ':' + toPort
-    });
-  });
+      // You can define here your custom logic to handle the request and then proxy the request.
+      myProxy.web(req, res, {
+        target: `http://${toHost}:${toPort}`
+      });
+    }
+  );
 
-  console.log('Mock keycloak-proxy listening on ' + fromHost + ':' + fromPort);
+  console.log(`Mock keycloak-proxy listening on ${fromHost}:${fromPort}`);
   server.listen(fromPort, fromHost);
   return server;
 };
