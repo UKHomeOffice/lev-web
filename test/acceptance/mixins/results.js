5afd3d847b7034b497e762d244c5e5099ddf39b8
Tim Gent <tim.gent@gmail.com>
2016-08-19 16:37:04 +0100

    Fixup

--- test/acceptance/mixins/results.js
+++ test/acceptance/mixins/results.js
@@ -7,7 +7,6 @@ module.exports = (target) => {
 
   target.clickFirstRecord = function() {
     // browser.element gets just the first matching elements (browser.elements gets all matching ones)
-    const linkToFirstRecordDetails = browser.element('#records a').value.ELEMENT;
-    this.elementIdClick(linkToFirstRecordDetails);
+    const linkToFirstRecordDetails = browser.element('#records a').click();
   };
 };
