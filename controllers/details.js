0f7bb6e4227e281860f7409df19b413b2822f26d
Robin Knipe <robin.knipe@gmail.com>
2016-10-19 12:10:52 +0100

    Extract and test error handling
    Make the module and test code more "promisey", and general tidy up.

--- controllers/details.js
+++ controllers/details.js
@@ -5,38 +5,34 @@ const helpers = require('../lib/helpers');
 const fields = require('../fields');
 const _ = require('lodash');
 
+const handleError = (err, next) => {
+  if (err.name === 'NotFoundError') {
+    return next();
+  }
+
+  return next(err instanceof(Error) ? err : new Error(err));
+};
+
 module.exports = function renderDetails(req, res, next) {
   req.params = req.params || {};
-
   const systemNumber = req.params.sysnum;
 
   if (systemNumber === undefined) {
-    next(new ReferenceError('The parameter \'id\' was not defined'), req, res);
-  } else if (!systemNumber.match(/^[0-9]+$/)) {
-    next(new TypeError('The parameter \'id\' was not an integer'), req, res);
-  } else {
-    const username = req.headers['X-Auth-Username'] || req.headers['x-auth-username'];
-    const canRedirectToResults = (req.query && req.query.multipleResults) !== undefined;
-
-    const promise = api.findBySystemNumber(Number(systemNumber), username);
-
-    promise
-      .then((result) => {
-          res.render('pages/details', {
-              record: result,
-              querystring: helpers.serialize(_.pick(req.query, _.keys(fields))),
-              canRedirectToResults: canRedirectToResults
-            });
-        }, (err) => {
-          if (err.name === 'NotFoundError') {
-            next();
-          } else {
-            const error = err instanceof(Error)
-              ? err
-              : new Error(err);
-
-            next(error, req, res, next);
-          }
-        });
+    return next(new ReferenceError('The parameter \'id\' was not defined'), req, res);
   }
+  if (!systemNumber.match(/^[0-9]+$/)) {
+    return next(new TypeError('The parameter \'id\' was not an integer'), req, res);
+  }
+
+  const username = req.headers['X-Auth-Username'] || req.headers['x-auth-username'];
+  const canRedirectToResults = (req.query && req.query.multipleResults) !== undefined;
+
+  return api.findBySystemNumber(Number(systemNumber), username)
+    .then(result => res.render('pages/details', {
+          record: result,
+          querystring: helpers.serialize(_.pick(req.query, _.keys(fields))),
+          canRedirectToResults: canRedirectToResults
+        }),
+        err => handleError(err, next)
+    );
 };
--- test/unit/controllers/details.js
+++ test/unit/controllers/details.js
@@ -1,43 +1,63 @@
 'use strict';
 
+const rewire = require('rewire');
+const detailsController = rewire('../../../controllers/details');
+const api = detailsController.__get__('api'); // eslint-disable-line no-underscore-dangle
+
 describe('controllers/details', function() {
-  var apiFindBySystemNumberStub;
-  var api;
-  var detailsController;
+  var req;
+  var res;
+  var next;
 
   beforeEach(sinon.test(function() {
-    apiFindBySystemNumberStub = this.stub();
+    var apiFindBySystemNumberStub = this.stub();
     apiFindBySystemNumberStub.withArgs(1234, 'mrs-caseworker').returns(Promise.resolve({ records: [] }));
     apiFindBySystemNumberStub.withArgs(34404, 'mrs-caseworker').returns(Promise.reject('error'));
-
-    detailsController = require('../../../controllers/details');
-
-    api = require('../../../api');
     api.findBySystemNumber = apiFindBySystemNumberStub;
+
+    req = {
+      params: {
+        sysnum: '1234'
+      },
+      headers: {
+        'X-Auth-Username': 'mrs-caseworker'
+      }
+    };
+
+    res = {
+      render: this.spy(),
+      redirect: this.spy()
+    };
+
+    next = this.spy();
   }));
 
+  describe('handleError function', () => {
+    const fn = detailsController.__get__('handleError'); // eslint-disable-line no-underscore-dangle
+
+    it('should pass through to the standard not found error handler on 404', () => {
+      const err = new Error('404 Not Found');
+      err.name = 'NotFoundError';
+      fn(err, next);
+      expect(next).to.have.been.calledWithExactly();
+    });
+
+    it('should pass through to the standard error handler when a specific error occurs', () => {
+      const err = new Error('Any old error');
+      fn(err, next);
+      expect(next).to.have.been.calledWithExactly(err);
+    });
+
+    it('should pass through to the standard error handler and wrap non error types', () => {
+      const err = 'Any old message';
+      fn(err, next);
+      expect(next.getCall(0).args[0])
+        .to.be.an('error')
+        .that.has.property('message', err);
+    });
+  });
+
   describe('when called', function() {
-    var req;
-    var res;
-    var next;
-
-    beforeEach(sinon.test(function() {
-      req = {
-        params: {
-          sysnum: '1234'
-        },
-        headers: {
-          'X-Auth-Username': 'mrs-caseworker'
-        }
-      };
-
-      res = {
-        render: this.spy(),
-        redirect: this.spy()
-      };
-
-      next = this.spy();
-    }));
 
     it('calls the api with the request GET params', function() {
       detailsController(req, res, next);
@@ -53,26 +73,21 @@ describe('controllers/details', function() {
 
     describe('resolved promise', function() {
 
-      it('renders the details page', function() {
-        detailsController(req, res, next);
-
-        return Promise.resolve().then(function() {
-          res.render.should.have.been.calledWith('pages/details');
-        });
-      });
+      it('renders the details page', () =>
+        detailsController(req, res, next).then(() =>
+          expect(res.render).to.have.been.calledWith('pages/details')
+        )
+      );
 
     });
 
     describe('rejected promise', function() {
 
       it('renders the error page', function() {
-        req.params.sysnum = 'error';
-
-        detailsController(req, res, next);
-
-        return Promise.resolve().then(function() {
-          next.should.have.been.calledWith(new Error('error'));
-        });
+        req.params.sysnum = '34404';
+        detailsController(req, res, next).then(() =>
+          expect(next).to.have.been.calledWith(new Error('error'))
+        );
       });
 
     });
