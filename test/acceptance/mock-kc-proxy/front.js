e4fe739b96b8f11ca2e26503a9dfed54008874da
Robin Knipe <robin.knipe@gmail.com>
2016-10-19 12:20:09 +0100

    Gracefully shutdown mock keycloak proxy
    Helps to significantly reduce the confusing NPM test output.

--- test/acceptance/mock-kc-proxy/front.js
+++ test/acceptance/mock-kc-proxy/front.js
@@ -1,6 +1,21 @@
 'use strict';
 
+process.title = 'lev-keycloak-proxy';
 const proxy = require('./');
 
-proxy('localhost', 8002, 'localhost', 8001, 'lev-e2e-tests');
+const server = proxy('localhost', 8002, 'localhost', 8001, 'lev-e2e-tests');
 
+// gracefully handle shutdowns -----------------------
+
+const closeGracefully = (signal) => {
+  setTimeout(() => {
+    console.warn('Forcefully shutting down from sig:', signal);
+    process.exit(0); // eslint-disable-line no-process-exit
+  }, 500);
+
+  server.close(() => process.exit(0)); // eslint-disable-line no-process-exit
+};
+
+['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach(signal =>
+  process.on(signal, () => closeGracefully(signal))
+);
