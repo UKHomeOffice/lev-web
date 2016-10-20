4035484c5aa0759ecd055122f0db65dbcd747210
Robin Knipe <robin.knipe@gmail.com>
2016-10-20 15:22:21 +0100

    Make mock API docker build a blocking job
    In Travis CI the mock API build job is currently non-blocking and does not complete in time for the acceptance tests to make use of the mock, so this will make it a blocking task, and should also be completed earlier in the CI process.

--- api/mock/get_latest_api_spec.sh
+++ api/mock/get_latest_api_spec.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-TARGET_DIR="./lev-api-docs"
+TARGET_DIR="${SCRIPT_DIR}/lev-api-docs"
 GIT_REPO=https://github.com/UKHomeOffice/lev-api-docs.git
-cd $SCRIPT_DIR
+cd ${SCRIPT_DIR}
 
 if [ -d "${TARGET_DIR}" ]
 then
     cd "${TARGET_DIR}"
     git pull
-    cd "${SCRIPT_DIR}"
 else
     git clone "${GIT_REPO}" || exit 1
 fi
 
 # straight-up force scripts to have exec permissions to fix GIT stupidity!
-find lev-api-docs/ -type f -name '*.sh' -exec chmod +x {} \;
+find ${TARGET_DIR} -type f -name '*.sh' -exec chmod +x {} \;
+
+${TARGET_DIR}/mock/build_mock_api_docker.sh
