5f888e9e5d55c3ad1e441f91ebb802b3da4bc34d
Robin Knipe <robin.knipe@gmail.com>
2016-11-01 17:15:03 +0000

    Make health endpoints available beyond localhost

--- test/unit/lib/health.js
+++ test/unit/lib/health.js
@@ -15,22 +15,12 @@ describe('Health check endpoints', () => {
     res = reqres.res();
   });
 
-  it('should respond with 200 - OK to local requests', done => {
-    req.ip = '127.0.0.1';
+  it('should respond with 200 - OK to all requests', done => {
     fn(req, res, err => {
       done(err || new Error('next function should not be called!'));
     });
     res.on('end', () => {
       expect(res.send).to.have.been.calledWith('OK');
-      done();
-    });
-  });
-
-  it('should do nothing when the request is not from localhost', done => {
-    req.ip = '1.1.1.1';
-    fn(req, res, err => {
-      expect(err).to.equal(undefined);
-      expect(res.send).not.to.have.been.called;
       done();
     });
   });
