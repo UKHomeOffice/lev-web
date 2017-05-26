920592777a254013b8497cc626cdffeac671babe
Robin Knipe <robin.knipe@gmail.com>
2017-05-26 11:45:29 +0100

    Make the download button work
    Scrape the data from the report table and send it to the user as a CSV file.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,37 @@
 'use strict';
 
 require('./details.polyfill')();
+var saveAs = require('file-saver').saveAs;
+
+function getAuditData() {
+  return Array.prototype.map.call(
+    document.querySelectorAll('table.audit tr'),
+    function parseRows(tr) {
+      return Array.prototype.map.call(
+        tr.children,
+        function parseCells(l) {
+          return l.textContent;
+        }
+      ).join(',');
+    }
+  ).join('\n');
+}
+
+function saveData(event) {
+  if (event.type === 'click' ||
+    (event.type === 'keydown' && (event.code === 'Enter' || event.key === 'Enter' || event.keyCode === 13))) {
+    event.preventDefault();
+    saveAs(
+      new Blob([getAuditData()], { type: 'text/plain;charset=' + document.characterSet }),
+      document.getElementById('save').download
+    );
+  }
+}
+
+document.getElementById('save').addEventListener('click', saveData, false);
+document.getElementById('save').addEventListener('keydown', saveData, false);
+
+module.exports = {
+  getAuditData: getAuditData,
+  saveData: saveData
+};
---
+++ test/unit/ui/index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const jsdom = require('jsdom-global');
+const fixture = fs.readFileSync(path.resolve(__dirname, 'fixture.html'));
+const output = fs.readFileSync(path.resolve(__dirname, 'fixture.csv')).toString();
+const proxyquire = require('proxyquire');
+
+
+describe('auidt report UI', () => {
+  const search = '?from=190117&to=290117&user=';
+  const url = `http://lev.com/${search}`;
+  let saver = sinon.spy();
+
+  let cleanup = jsdom(fixture, {
+    url: url,
+    contentType: 'text/html'
+  });
+  let index = proxyquire('../../../assets/js/index.js', {
+    './details.polyfill': sinon.spy(),
+    'file-saver': {
+      saveAs: saver
+    }
+  });
+
+  after('cleanup', () => {
+    cleanup();
+  });
+
+  it('has location object and search property', () => {
+    expect(global.location).has.property('search', search);
+  });
+
+  describe('#getAuditData', () => {
+    it('should be a function', () => {
+      expect(index.getAuditData).to.be.a('function');
+    });
+
+    it('should produce CSV data', () => {
+      expect(index.getAuditData()).to.equal(output);
+    });
+  });
+
+  describe('#getAuditData', () => {
+    beforeEach('reset saver spy', () => {
+      saver.reset();
+    });
+
+    it('should be a function', () => {
+      expect(index.saveData).to.be.a('function');
+    });
+
+    it('should produce CSV data after a click', () => {
+      const event = {
+        preventDefault: sinon.spy(),
+        type: 'click'
+      };
+      index.saveData(event);
+
+      expect(event.preventDefault).to.have.been.calledOnce;
+      expect(saver).to.have.been.calledOnce
+        .and.to.have.been.calledWith(
+          new global.Blob(['something else'], { type: 'text/plain;charset=UTF-8' }),
+          'audit_report_19-01-17_to_29-01-17.csv'
+        );
+    });
+
+    it('should produce CSV data after the Enter key is pressed', () => {
+      const event = {
+        preventDefault: sinon.spy(),
+        type: 'keydown',
+        key: 'Enter',
+        code: 'Enter',
+        keyCode: 13
+      };
+      index.saveData(event);
+
+      expect(event.preventDefault).to.have.been.calledOnce;
+      expect(saver).to.have.been.calledOnce
+        .and.to.have.been.calledWith(
+        new global.Blob(['something else'], { type: 'text/plain;charset=UTF-8' }),
+        'audit_report_19-01-17_to_29-01-17.csv'
+      );
+    });
+
+    it('should produce CSV data after a click', () => {
+      const event = {
+        preventDefault: sinon.spy(),
+        type: 'keydown',
+        key: ' ',
+        Code: 'Space',
+        keyCode: 32
+      };
+      index.saveData(event);
+
+      expect(event.preventDefault).not.to.have.been.called;
+      expect(saver).not.to.have.been.called;
+    });
+  });
+
+});
