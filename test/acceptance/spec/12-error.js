9aaf9422b798e1c4103a046c4ddf32b894b059d1
Robin Knipe <robin.knipe@gmail.com>
2018-01-26 17:40:49 +0000

    [LEVWEB-873] Add test description to check fix
    Make sure the "Start Again" link goes back to the Audit Report as expected.

--- test/acceptance/spec/12-error.js
+++ test/acceptance/spec/12-error.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// const moment = require('moment');
-// const testConfig = require('../config');
-// const env = testConfig.env;
-
 describe('Error page', () => {
 
   describe('shown after a birth search error', () => {
@@ -15,13 +11,33 @@ describe('Error page', () => {
       browser.shouldBeOn404Page();
     });
 
-    describe('clicking the Start Again link', () => {
+    describe('has a Start Again link which', () => {
       before('click the Start Again link', () => {
         browser.click('a.button');
       });
 
       it('should take the user back to the Birth Search page', () => {
         browser.shouldBeOnSearchPage();
+      });
+    });
+  });
+
+  describe('shown after an audit report search error', () => {
+    before('bad audit search', () => {
+      browser.doBadAuditSearch();
+    });
+
+    it('should show the error page', () => {
+      browser.shouldBeOnErrorPage();
+    });
+
+    describe('has a Start Again link which', () => {
+      before('click the Start Again link', () => {
+        browser.click('a.button');
+      });
+
+      it('should take the user back to the User Activity Report page', () => {
+        browser.shouldBeOnUserActivityReport();
       });
     });
   });
