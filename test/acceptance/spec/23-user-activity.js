173e78ec574d6426bfc0ef8affc98540c99276f2
Robin Knipe <robin.knipe@gmail.com>
2018-01-26 18:48:11 +0000

    [LEVWEB-871] Add placeholder acceptance tests
    That can be "un-`skip`'d" once the validation problem of referring to multiple fields has been worked out...

--- test/acceptance/spec/23-user-activity.js
+++ test/acceptance/spec/23-user-activity.js
@@ -370,6 +370,24 @@ describe('User Activity', () => {
       });
     });
 
+    // placeholder test for making search date range limit check part of validation, instead of a 500 error
+    describe.skip(`with a date range greater than ${testConfig.MAX_AUDIT_RANGE} days`, () => {
+      before(() => {
+        browser.generateReport(
+          moment().subtract(testConfig.MAX_AUDIT_RANGE + 1, 'days').format('DD/MM/YYYY'),
+          moment().format('DD/MM/YYYY')
+        );
+      });
+
+      it('displays an error message', () => {
+        browser.getText('h2').should.contain('Fix the following error');
+      });
+
+      it('requests a reduced date range', () => {
+        browser.getText('a').should.contain(`Please enter a date range less than ${testConfig.MAX_AUDIT_RANGE} days`);
+      });
+    });
+
     describe('with invalid characters in the user search filter', () => {
       before(() => {
         browser.generateReport(
