c0d491bcff8b010c4b9f9236614b6522ab6b030a
Daniel A.C. Martin <github@daniel-martin.co.uk>
2018-03-03 01:04:33 +0000

    Migrate to new mock
    Use the new mock based on the new Node.js-based API instead of the old
    Wiremock-based one.

--- test/acceptance/mock-kc-proxy/front.js
+++ test/acceptance/mock-kc-proxy/front.js
@@ -3,9 +3,11 @@
 process.title = 'lev-keycloak-proxy';
 const proxy = require('./');
 const proxyHost = process.env.PROXY_HOST || 'localhost';
+const proxyPort = process.env.PROXY_PORT || 8001;
 const listenHost = process.env.LISTEN_HOST || 'localhost';
+const listenPort = process.env.LISTEN_PORT || 8002;
 
-const server = proxy(listenHost, 8002, proxyHost, 8001, '<audit-token>');
+const server = proxy(listenHost, listenPort, proxyHost, proxyPort, '<audit-token>');
 
 // gracefully handle shutdowns -----------------------
 
