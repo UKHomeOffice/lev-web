9cbcecd110341656c28d07fe0566a8b1a173fbba
Robin Knipe <robin.knipe@gmail.com>
2018-06-21 15:38:37 +0100

    Fix linting issues

--- test/acceptance/spec/20-details.js
+++ test/acceptance/spec/20-details.js
@@ -4,11 +4,9 @@ var expectedRecord = require('../expected-record');
 var expectedRecords = require('../expected-records');
 
 describe('Details Page', () => {
-  const urlShouldContainDetails = () =>
-    it('returns a details page', () => browser.shouldBeOnDetailsPage());
+  const urlShouldContainDetails = () => it('returns a details page', () => browser.shouldBeOnDetailsPage());
 
-  const urlShouldContainSearch = () =>
-    it('returns the search page', () => browser.shouldBeOnSearchPage());
+  const urlShouldContainSearch = () => it('returns the search page', () => browser.shouldBeOnSearchPage());
 
   const messageDisplayed = (recordToMatch) => {
     it('an appropriate message is displayed', () => {
@@ -17,27 +15,27 @@ describe('Details Page', () => {
     });
   };
 
-  const recordDisplayed = (recordToMatch) => {
+  const recordDisplayed = (record) => {
     it('the complete record is displayed in a table', () => {
       const browserText = browser.$$('table tr');
       // Regexes used here as htmlunit and chrome differ in showing space so need regex to work with both
-      browserText[0].getText().should.match(new RegExp('System number *' + recordToMatch.systemNumber));
-      browserText[2].getText().should.match(new RegExp('Surname *' + recordToMatch.child.name.surname));
-      browserText[3].getText().should.match(new RegExp('Forename\\(s\\) *' + recordToMatch.child.name.givenName));
-      browserText[4].getText().should.match(new RegExp('Date of birth *' + recordToMatch.child.dateOfBirth));
-      browserText[5].getText().should.match(new RegExp('Sex *' + recordToMatch.child.sex));
-      browserText[6].getText().should.match(new RegExp('Place of birth *' + recordToMatch.child.birthplace));
-      browserText[8].getText().should.match(new RegExp('Name *' + recordToMatch.mother.name.fullName));
-      browserText[9].getText().should.match(new RegExp('Maiden name *' + recordToMatch.mother.maidenSurname));
-      browserText[10].getText().should.match(new RegExp('Previous marriage name *' + recordToMatch.mother.marriageSurname));
-      browserText[11].getText().should.match(new RegExp('Place of birth *' + recordToMatch.mother.birthplace));
-      browserText[13].getText().should.match(new RegExp('Name *' + recordToMatch.father.name.fullName));
-      browserText[14].getText().should.match(new RegExp('Place of birth *' + recordToMatch.father.birthplace));
+      browserText[0].getText().should.match(new RegExp('System number *' + record.systemNumber));
+      browserText[2].getText().should.match(new RegExp('Surname *' + record.child.name.surname));
+      browserText[3].getText().should.match(new RegExp('Forename\\(s\\) *' + record.child.name.givenName));
+      browserText[4].getText().should.match(new RegExp('Date of birth *' + record.child.dateOfBirth));
+      browserText[5].getText().should.match(new RegExp('Sex *' + record.child.sex));
+      browserText[6].getText().should.match(new RegExp('Place of birth *' + record.child.birthplace));
+      browserText[8].getText().should.match(new RegExp('Name *' + record.mother.name.fullName));
+      browserText[9].getText().should.match(new RegExp('Maiden name *' + record.mother.maidenSurname));
+      browserText[10].getText().should.match(new RegExp('Previous marriage name *' + record.mother.marriageSurname));
+      browserText[11].getText().should.match(new RegExp('Place of birth *' + record.mother.birthplace));
+      browserText[13].getText().should.match(new RegExp('Name *' + record.father.name.fullName));
+      browserText[14].getText().should.match(new RegExp('Place of birth *' + record.father.birthplace));
       browserText[16].getText().should.match(new RegExp('Birth registered by *Mother'));
-      browserText[17].getText().should.match(new RegExp('Registration district *' + recordToMatch.registrationDistrict));
-      browserText[18].getText().should.match(new RegExp('Sub-district *' + recordToMatch.subDistrict));
-      browserText[19].getText().should.match(new RegExp('Administrative area *' + recordToMatch.administrativeArea));
-      browserText[20].getText().should.match(new RegExp('Date of registration *' + recordToMatch.date));
+      browserText[17].getText().should.match(new RegExp('Registration district *' + record.registrationDistrict));
+      browserText[18].getText().should.match(new RegExp('Sub-district *' + record.subDistrict));
+      browserText[19].getText().should.match(new RegExp('Administrative area *' + record.administrativeArea));
+      browserText[20].getText().should.match(new RegExp('Date of registration *' + record.date));
     });
   };
 
