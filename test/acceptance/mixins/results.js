90f293d37ac3af8a8de863f52bb232e105f67238
Daniel A.C. Martin <github@daniel-martin.co.uk>
2018-09-18 13:51:05 +0100

    Make results page tests more strict

--- test/acceptance/mixins/results.js
+++ test/acceptance/mixins/results.js
@@ -6,8 +6,16 @@ module.exports = (target) => {
   };
 
   target.shouldBeOnBirthResultsPage = target.shouldBeOnResultsPage;
-  target.shouldBeOnDeathResultsPage = target.shouldBeOnResultsPage;
-  target.shouldBeOnMarriageResultsPage = target.shouldBeOnResultsPage;
+
+  target.shouldBeOnDeathResultsPage = function() {
+    this.getUrl().should.contain('/death');
+    target.shouldBeOnResultsPage();
+  };
+
+  target.shouldBeOnMarriageResultsPage= function() {
+    this.getUrl().should.contain('/marriage');
+    target.shouldBeOnResultsPage();
+  };
 
   target.clickFirstRecord = function() {
     // browser.element gets just the first matching elements (browser.elements gets all matching ones)
