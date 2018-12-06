ca381697c2e539e60502edd5847e31a7a0b5c827
Robin Knipe <robin.knipe@gmail.com>
2018-12-06 12:59:54 +0000

    [LEVWEB-1147] Search marriages with groups header
    ...to allow spoofing of a user in a specific group that should only see the limited version of the marriage information (e.g. "MHCTS").

--- test/acceptance/mixins/search.js
+++ test/acceptance/mixins/search.js
@@ -20,6 +20,19 @@ module.exports = (target) => {
     this.submitMarriageSearchPage(systemNumber, surname, forenames, dom);
   };
 
+  target.marriageSearchWithGourpHeader = function(groups) {
+    this.execute((u, g) => {
+      /* eslint-env browser */
+      var client = new XMLHttpRequest();
+      client.open('GET', u, false);
+      client.setRequestHeader('X-Auth-Groups', g);
+      client.send();
+      document.body.parentElement.innerHTML = client.responseText;
+      /* eslint-env node, mocha */
+    }, this.getUrl(), groups);
+    this.waitForExist('table.details', 5000);
+  };
+
   target.goToSearchPage = function() {
     this.url(url);
   };
