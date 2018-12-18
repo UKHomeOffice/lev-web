1b16ef62ea8158d07ca85fdd242e90cf58b34af7
Daniel A.C. Martin <github@daniel-martin.co.uk>
2018-12-18 23:57:23 +0000

    Make new variable names more descriptive
    Replaces 'full data' with things that are more appropriate in the given
    context.

--- controllers/marriage-details.js
+++ controllers/marriage-details.js
@@ -18,7 +18,7 @@ module.exports = function renderDetails(req, res, next) {
   req.params = req.params || {};
   const systemNumber = req.params.sysnum;
   const ri = reqInfo(req);
-  const fullData = ri.roles.filter(v => v === 'full-data').reduce((acc, val) => acc || val, false) && true;
+  const fullDetails = ri.roles.filter(v => v === 'full-details').reduce((acc, val) => acc || val, false) && true;
 
   if (systemNumber === undefined) {
     return next(new ReferenceError('The parameter \'id\' was not defined'), req, res);
@@ -33,7 +33,7 @@ module.exports = function renderDetails(req, res, next) {
   return api.findMarriageBySystemNumber(Number(systemNumber), accessToken)
     .then(result => res.render('pages/marriage-details', {
         record: result,
-        fullData: fullData,
+        showAll: fullDetails,
         querystring: helpers.serialize(_.pick(req.query, _.keys(fields))),
         canRedirectToResults: canRedirectToResults
       }),
