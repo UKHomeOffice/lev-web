4cc4e278626e16d64b7e4f3d8ea06252d49730f6
Robin Knipe <robin.knipe@gmail.com>
2018-12-21 19:40:00 +0000

    [LEVWEB-1173] Functional test marriage-details

---
+++ test/unit/controllers/marriage-details.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const rewire = require('rewire');
+const detailsController = rewire('../../../controllers/marriage-details');
+const api = detailsController.__get__('api'); // eslint-disable-line no-underscore-dangle
+
+const accessToken = 'accessToken';
+
+describe('controllers/marriage-details', function() {
+  let req;
+  let res;
+  let next;
+
+  beforeEach(sinon.test(function() {
+    const apiFindBySystemNumberStub = this.stub();
+    apiFindBySystemNumberStub.withArgs(1234, accessToken).returns(Promise.resolve({ records: [] }));
+    apiFindBySystemNumberStub.withArgs(34404, accessToken).returns(Promise.reject('error'));
+    api.findMarriageBySystemNumber = apiFindBySystemNumberStub;
+
+    req = {
+      params: {
+        sysnum: '1234'
+      },
+      headers: {
+        'X-Auth-Token': accessToken
+      }
+    };
+    res = {
+      render: this.spy(),
+      redirect: this.spy()
+    };
+    next = this.spy();
+  }));
+
+  describe('handleError function', () => {
+    const fn = detailsController.__get__('handleError'); // eslint-disable-line no-underscore-dangle
+
+    it('should pass through to the standard not found error handler on 404', () => {
+      const err = new Error('404 Not Found');
+      err.name = 'NotFoundError';
+      fn(err, next);
+      expect(next).to.have.been.calledWithExactly();
+    });
+
+    it('should pass through to the standard error handler when a specific error occurs', () => {
+      const err = new Error('Any old error');
+      fn(err, next);
+      expect(next).to.have.been.calledWithExactly(err);
+    });
+
+    it('should pass through to the standard error handler and wrap non error types', () => {
+      const err = 'Any old message';
+      fn(err, next);
+      expect(next.getCall(0).args[0])
+        .to.be.an('error')
+        .that.has.property('message', err);
+    });
+  });
+
+  describe('renderDetails function', function() {
+    it('calls the api with the request GET params', function() {
+      detailsController(req, res, next);
+
+      api.findMarriageBySystemNumber.should.have.been.calledWith(1234, accessToken);
+    });
+
+    it('raises an error with no GET params', function() {
+      detailsController({}, res, next);
+
+      next.should.have.been.calledWith(new ReferenceError());
+    });
+
+    describe('resolved promise', function() {
+      it('renders the details page', () =>
+        detailsController(req, res, next).then(() =>
+          expect(res.render).to.have.been.calledWith('pages/marriage-details')
+            .and.to.have.deep.property('args[0][1]').that.is.an('object').and.has.property('showAll').that.is.false
+        )
+      );
+    });
+
+    describe('rejected promise', function() {
+      it('renders the error page', function() {
+        req.params.sysnum = '34404';
+        detailsController(req, res, next).then(() =>
+          expect(next).to.have.been.calledWith(new Error('error'))
+        );
+      });
+    });
+  });
+});
