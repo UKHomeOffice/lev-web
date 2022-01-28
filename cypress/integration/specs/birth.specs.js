4733e3aa1064c3a0574b43134b362ff916d2dea2
JamesWoodwardHMPO <James.Woodward@digital.homeoffice.gov.uk>
2022-01-28 09:49:13 +0000

    Added cypress framework and POC of cypress-keycloak

---
+++ cypress/integration/specs/birth.specs.js
@@ -0,0 +1,50 @@
+describe('My first test', () => {
+
+  it('Test LOCAL, Should display the birth page', () => {
+    cy.visit('http://localhost:8001');
+
+    cy.contains('Birth record check');
+
+    cy.get('#system-number')
+      .type('123456789');
+
+    cy.get('input[type="submit"]')
+      .click();
+
+    cy.url()
+      .should('include', 'details/123456789?system-number=123456789');
+  });
+
+  it('Test DEV, Should display the birth page', () => {
+    cy.logout({
+      root: 'https://sso-dev.notprod.homeoffice.gov.uk',
+      realm: 'lev',
+      // eslint-disable-next-line camelcase
+      redirect_uri: 'https://dev.notprod.lev.homeoffice.gov.uk'
+    });
+
+    cy.login({
+      root: 'https://sso-dev.notprod.homeoffice.gov.uk',
+      realm: 'lev',
+      // eslint-disable-next-line camelcase
+      redirect_uri: 'https://dev.notprod.lev.homeoffice.gov.uk',
+      // eslint-disable-next-line camelcase
+      client_id: 'lev-web',
+      username: 'James.Woodward@digital.homeoffice.gov.uk',
+      password: 'Lev28oct'
+    });
+
+    cy.visit('https://dev.notprod.lev.homeoffice.gov.uk/');
+
+    cy.contains('Birth record check');
+
+    cy.get('#system-number')
+      .type('123456789');
+
+    cy.get('input[type="submit"]')
+      .click();
+
+    cy.url()
+      .should('include', 'details/123456789?system-number=123456789');
+  });
+});
