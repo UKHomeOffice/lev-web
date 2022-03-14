de515eef00c65f6852cb2be3121f49b73e752567
James Woodward <james.woodward@digital.homeoffice.gov.uk>
2022-03-14 10:01:21 +0000

    feat(cypress): add death-details tests

--- cypress/pages/death/DeathDetailsPage.js
+++ cypress/pages/death/DeathDetailsPage.js
@@ -23,6 +23,44 @@ class DeathDetailsPage extends DetailsPage {
    * Check death registrations details page has the expected data
    */
   static hasExpectedRecord(record) {
+    if (Cypress.env('env') === 'dev') {
+      this.hasCompleteRecord(record);
+    } else {
+      this.hasLimitedRecord(record);
+    }
+  }
+
+  /**
+   * Check death registrations details page has the expected data
+   */
+  static hasLimitedRecord(record) {
+    const { deceased, registrar } = record;
+    const rows = [
+      `System number ${record.id}`,
+
+      'Deceased',
+      `Name ${deceased.forenames} ${deceased.surname}`,
+      `Date of birth ${deceased.dateOfBirth}`,
+      `Sex ${deceased.sex}`,
+      `Address ${deceased.address}`,
+      `Date of death ${deceased.dateOfDeath}`,
+
+      'Registration',
+      `Sub-district ${registrar.subdistrict}`,
+      `District ${registrar.district}`,
+      `Administrative area ${registrar.administrativeArea}`,
+      `Date of registration ${record.date}`,
+    ];
+
+    rows.forEach((value, index) => {
+      cy.get('table tr').eq(index).contains(value);
+    });
+  }
+
+  /**
+   * Check death registrations details page has the expected data
+   */
+  static hasCompleteRecord(record) {
     const { deceased, informant, registrar } = record;
     const rows = [
       `System number ${record.id}`,
