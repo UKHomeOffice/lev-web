8a54626c51772aff119cf32d0776e9ebada9ff3e
Aaron Gill <aaron.gill@digital.homeoffice.gov.uk>
2022-03-18 15:06:03 +0000

    Partnership search tests migrated

---
+++ cypress/fixtures/partnership/expected-partnership-record.js
@@ -0,0 +1,124 @@
+const search = {
+  systemNumber: '',
+  forenames: 'Tester',
+  surname: 'SOLO',
+  dop: '29/02/2012',
+};
+
+const result = {
+  "id": 999999910,
+  "date": "10/10/2014",
+  "dateOfPartnership": "29/02/2012",
+  "placeOfPartnership": {
+    "address": "Test place",
+    "short": "Test place"
+  },
+  "registrar": {
+    "signature": "A. Registrar"
+  },
+  "partner1": {
+    "prefix": "Mr",
+    "forenames": "Tester",
+    "surname": "SOLO",
+    "suffix": "IV",
+    "dob": "29/02/1912",
+    "sex": "Female",
+    "occupation": "Unemployed",
+    "retired": false,
+    "address": "10 Test street",
+    "aliases": [
+      {
+        "prefix": "Mr",
+        "forenames": "Test Previous",
+        "surname": "Smithers",
+        "suffix": "IV"
+      },
+      {
+        "prefix": "Mr",
+        "forenames": "Testy Previous",
+        "surname": "Smithers",
+        "suffix": "IV"
+      }
+    ],
+    "signature": "T. Solo",
+    "condition": "Single"
+  },
+  "partner2": {
+    "prefix": "Miss",
+    "forenames": "Test",
+    "surname": "BRIDE",
+    "suffix": "IV",
+    "dob": "08/08/2008",
+    "sex": "Female",
+    "occupation": "Bride",
+    "retired": false,
+    "address": "A dark dark flat in a dark dark house, down a dark dark street, in a dark dark town",
+    "aliases": [
+      {
+        "prefix": "Ms",
+        "forenames": "Test Previous",
+        "surname": "Smithers",
+        "suffix": "IV"
+      },
+      {
+        "prefix": "Ms",
+        "forenames": "Testy Previous",
+        "surname": "Smithers",
+        "suffix": "IV"
+      }
+    ],
+    "signature": "T. Bride",
+    "condition": "Single"
+  },
+  "fatherOfPartner1": {
+    "forenames": "Test Fop1",
+    "surname": "FATHER",
+    "occupation": "Father",
+    "retired": true,
+    "designation": "Step-father",
+    "deceased": true
+  },
+  "motherOfPartner1": {
+    "forenames": "Serah Mop1",
+    "surname": "MOTHER",
+    "occupation": "Mother",
+    "retired": true,
+    "designation": "Stepmother",
+    "deceased": true
+  },
+  "fatherOfPartner2": {
+    "forenames": "Test Fop2",
+    "surname": "FATHER",
+    "occupation": "Father",
+    "retired": true,
+    "designation": "Father",
+    "deceased": true
+  },
+  "motherOfPartner2": {
+    "forenames": "Serah Mop2",
+    "surname": "MOTHER",
+    "occupation": "Mother",
+    "retired": true,
+    "designation": "Mother",
+    "deceased": true
+  },
+  "witness1": {
+    "forename": "Arthur",
+    "surname": "Witness"
+  },
+  "witness2": {
+    "forename": "Abernathy",
+    "surname": "Other"
+  },
+  "status": {
+    "blocked": false,
+    "marginalNotes": null
+  },
+  "nextRegistration": null,
+  "previousRegistration": null
+}
+
+module.exports = {
+  result,
+  search
+};
--- cypress/pages/partnership/PartnershipResultsPage.js
+++ cypress/pages/partnership/PartnershipResultsPage.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const ResultsPage = require('../ResultsPage');
+const multipleRecords = require("../../fixtures/partnership/expected-partnership-records");
+const {multipleValidRecords: expectedMultipleRec} = require("../../fixtures/birth");
 
 class PartnershipResultsPage extends ResultsPage {
 
@@ -10,6 +12,46 @@ class PartnershipResultsPage extends ResultsPage {
   static shouldBeVisible() {
     super.shouldBeVisible();
     cy.url().should('include', '/partnership');
+  }
+
+  static noRecordFound(record) {
+    cy.get('h1').contains(`No records found for ${record.forenames} ${record.surname} ${record.dop}`);
+  }
+
+  static multipleRecordsSummary(dop) {
+    const dateOfPartnership = dop ? dop : multipleRecords.result.dateOfPartnership;
+    const { surname, forenames } = multipleRecords.search;
+
+    cy.get('h1').contains(`4 records found for ${forenames} ${surname} ${dateOfPartnership}`);
+  }
+
+    static multipleRecordsFound() {
+      const { placeOfPartnership, dateOfPartnership, partner1, partner2 } = multipleRecords.result;
+
+      ResultsPage.shouldBeVisible();
+
+      // displays a subset of each record in a list
+      cy.get('ul>li').each(() => {})
+        .contains(`${partner1.forenames} ${partner1.surname} & ${partner2.forenames} ${partner2.surname}`);
+      cy.get('ul>li').each(() => {}).contains(`Date of civil partnership ${dateOfPartnership}`);
+      cy.get('ul>li').each(() => {}).contains(`Place of civil partnership ${placeOfPartnership.short}`);
+    }
+
+
+  static selectFirstRecord() {
+    cy.get('ul>li').eq(1).click();
+  }
+
+  static editSearchLinkDisplayed() {
+    cy.get('#editSearchLink').contains('Edit search');
+  }
+
+  static clickNewSearchLink() {
+    cy.get('#newSearchLink').click();
+  }
+
+  static clickEditSearchLink() {
+    cy.get('#editSearchLink').click();
   }
 }
 
